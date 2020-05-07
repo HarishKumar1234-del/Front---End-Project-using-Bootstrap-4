4484be8a3f98a6f1545bd60523419c170833edd7
harish kumar <dalchand4ud@gmail.com>
2020-05-08 00:03:37 +0530

    npm scripts part 2

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+    // read function gives error
+    // $('document').read(function(){
+        $("#mycarousel").carousel({ interval:2000 });
+        $("#carouselButton").click(function(){
+        	if($('#carouselButton').children('span').hasClass('fa-pause')){		
+            	$("#mycarousel").carousel("pause");
+            	$("#carouselButton").children('span').removeClass('fa-pause');
+            	$("#carouselButton").children('span').addClass('fa-play');
+
+        	}
+        	else if($('#carouselButton').children('span').hasClass('fa-play')){		
+            	$("#mycarousel").carousel("cycle");
+            	$("#carouselButton").children('span').removeClass('fa-play');
+            	$("#carouselButton").children('span').addClass('fa-pause');
+
+        	}
+        });
+        // $("#carousel-play").click(function(){
+        //     $("#mycarousel").carousel("cycle");
+        // });
+    // });
